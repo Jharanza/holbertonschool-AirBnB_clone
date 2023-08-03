cb760ed5921620e78aba1bfe85fee347e65c6b3e
MiguelBG12 <miguel.bg12@gmail.com>
2023-08-03 16:03:39 -0700

    Class to define all common attributes/methods

--- models/base_model.py
+++ models/base_model.py
@@ -9,25 +9,25 @@ import uuid
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-    def __ini__(self)
-    """Initialization of the base model"""
+    def __init__(self):
+        """Initialization of the base model"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now
-        self.update_at = self.created_at
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
 
     def __str__(self):
-        """String representatiion of the BaseModel class"""
+        """String representation of the BaseModel class"""
         return "[{:s}]({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the atribute 'updated_at' with the current datetime"""
+        """Updates the atribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """return a dictionary containing all keys/values of the instance"""
+        """Return a dictionary containing all keys/values of the instance"""
         time = "%Y-%m-%dT%H:%M:%S.%f"
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = new_dict["created_at"].strftime(time)
         new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] =self.__class__.__name
-        return new_dict   
+        new_dict["__class__"] =self.__class__.__name__
+        return new_dict 
