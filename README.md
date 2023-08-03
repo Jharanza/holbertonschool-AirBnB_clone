912148a9e3dad0c061ac923fec02669641b0f030
MiguelBG12 <miguel.bg12@gmail.com>
2023-08-02 21:45:25 -0700

    Adding unittests

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ All file will are written in Python programming language
 ### Usage
 The HBNB console wil have the interactive and non-interactive mode
 
-##Console Commands
+###Console Commands
 Command	Description
 all Prints all string representation of all instances based or not on the class name
 help Display help page
@@ -21,6 +21,10 @@ create <class> Creates a new instance of a given class with a unique ID and save
 destroy <class> <id> Deletes an instance based on the class name and id and saves it to a JSON file
 show <class> <id> Prints the string representation of a class instance based on the class name and ID
 
+### File Descriptions
+- *models* directory:
+  - **base_model.py:** Class *BaseModel* which all other classes inherit from. Attributes: *id, created_at, updated_at*.
+  - **city.py:** Class *City*. Attributes: *state_id, name*.
 ## Unit Testing
 Unit test are stored in the *tests* directory. To run unit test, run the following command:
 """
--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,52 @@
 #!/usr/bin/python3
-''' Module base for all '''
+import models
 from datetime import datetime
+from uuid import uuid
+
 
 class BaseModel:
-
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
-
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        my_dict = self.__dict__.copy()
-        my_dict['__clase__'] = self.__class__.__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+    """
+    Base clase
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize an instance of BaseModel
+        """
+        if kwargs:
+            for key, value in kwargs.items()
+                if key == "created_at" or key == "update_at":
+                    dttime_ob = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, dttime_ob)
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        text = "[{}] ({}) {}"
-        return text.format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        Return a string representation of BaseModel
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates the updated_at public instance attribute with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Return a dictionary containing all key/values of __dict__
+        """
+        dic = {}
+        dic["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, datetime):
+                dic[k] = v.isoformat()
+            else:
+                dic[k] = v
+        return dic
