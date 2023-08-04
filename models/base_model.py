898b72b48088346a92d7fe70ca21ac6643d9a601
jharanza <jharanza1986@gmail.com>
2023-08-04 11:00:11 -0700

    Adding comments to the methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-''' Module base for all '''
+''' Module base for the files of the proyect '''
 from datetime import datetime
 
-class BaseModel:
 
+class BaseModel:
+    ''' class that assign an id to an object, update
+    the date the time is called and manipulate the method
+    dict. Count with 4 methods:
+        constructor
+        save
+        to_dict
+        to_str
+    '''
     def __init__(self):
+        ''' method constructor that assign an unique id to an object '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
-
     def save(self):
+        ''' method public that update the attribute updates_at '''
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        ''' method public that manipulate the method dict() '''
         my_dict = self.__dict__.copy()
         my_dict['__clase__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
@@ -21,5 +31,6 @@ class BaseModel:
         return my_dict
 
     def __str__(self):
+        ''' method overwrite to represent the object '''
         text = "[{}] ({}) {}"
         return text.format(self.__class__.__name__, self.id, self.__dict__)
