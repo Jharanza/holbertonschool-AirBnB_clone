f3311239fa79aaf2cded45ac590d3c8b575ea7cc
MiguelBG12 <miguel.bg12@gmail.com>
2023-08-05 11:24:00 -0700

    Fixing code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
 
     def all(self):
         ''' return the object like a dictionary '''
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         '''
@@ -21,15 +21,17 @@ class FileStorage:
             Args:
                 obj: object to set
         '''
-        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        if obj:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         ''' method that seialize the object '''
-        file_obj = FileStorage.__objects
-        serial_obj = {key: obj.to_dict() for key, obj in file_obj.items()}
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(serial_obj, file)
+        data = {}
+        for k, v in self_obj.items()
+            data[k] = v.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(data, file)
 
     def reload(self):
         ''' method that deserialize the object '''
@@ -38,9 +40,9 @@ class FileStorage:
             "BaseModel": BaseModel,
         }
         try:
-            with open(self.__file_path, 'r') as file:
-                data_dict = json.load(file)
-                for key, value in data_dict.items():
-                    self.__objects[key] = classes["__class__"]](**value)
+            with open(self.__file_path, 'r') as f:
+                data_dict = json.load(f)
+                for k, v in data_dict.items():
+                    self.__objects[k] = classes["__class__"]](**v)
         except FileNotFoundError:
             pass
