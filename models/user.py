ad2ccac4da013bb105b974b1b093db2143dac331
jharanza <jharanza1986@gmail.com>
2023-08-06 11:33:48 -0700

    correcting the words

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     ''' class User with public attributes inherits from BaseModel'''
-    def __init__(self, *args, **kvargs):
+    def __init__(self, *args, **kwargs):
         ''' Call the emthod super '''
         super().__init__(*args, **kwargs)
         self.email = ''
