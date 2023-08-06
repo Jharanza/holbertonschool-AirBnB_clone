3d643822ee481000c7c05741db03a1ba2016d0ef
jharanza <jharanza1986@gmail.com>
2023-08-06 11:31:00 -0700

    Correcting a letter

--- models/user.py
+++ models/user.py
@@ -6,8 +6,8 @@ from models.base_model import BaseModel
 class User(BaseModel):
     ''' class User with public attributes inherits from BaseModel'''
     def __init__(self, *args, **kvargs):
-        ''' Using emthod super to call '''
-        super().__init__(*args, **Kwargs)    
+        ''' Call the emthod super '''
+        super().__init__(*args, **kwargs)
         self.email = ''
         self.password = ''
         self.first_name = ''
