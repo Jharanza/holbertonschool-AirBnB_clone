b5e0a74bcff325b97491cc1e6577a696594baa94
jharanza <jharanza1986@gmail.com>
2023-08-06 12:58:13 -0700

    Uploading files to instance objects

---
+++ models/place.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' module place that inherits from BseModel'''
+
+
+class Place(BaseModel):
+    ''' clase Place '''
+    city_id = ''
+    user_id = ''
+    name = ''
+    description = ''
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' module review that inherits from BaseModel '''
+
+
+class Review(BaseModel):
+    ''' class Review '''
+    place_id = ''
+    user_id = ''
+    text = ''
