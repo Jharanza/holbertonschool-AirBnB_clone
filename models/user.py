149e7daf436b80b81da477190bde3d2a76de3cec
MiguelBG12 <miguel.bg12@gmail.com>
2023-08-06 15:28:42 -0700

    Adding user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-''' module to instance a user '''
 from models.base_model import BaseModel
-
+"""Module to instance a user"""
 
 class User(BaseModel):
-    ''' class User with public attributes inherits from BaseModel'''
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """class User with public attributes inherits from BaseModel
+
+    Attributes:
+    email: email of User
+    password: password of User
+    First_name: first name of User
+    last_name: last name of User
+    """
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
