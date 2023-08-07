921d8325b754b9e0a9f09f0e17d1c0e428f145a6
MiguelBG12 <miguel.bg12@gmail.com>
2023-08-06 18:40:10 -0700

    Update new classes in the console.

--- console.py
+++ console.py
@@ -5,7 +5,11 @@ import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.user import User
-
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.state import State
 
 class HBNBCommand(cmd.Cmd):
     """Interpreter class from cmd"""
